8ac51c8ff693bc77c8e98c2152097ce25b6d455a
alishazia <shaziaali812@gmail.com>
2020-07-27 12:11:55 +0530

    hoisting concept

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 
 // Shallow Copy and Deep Copy 
-const a = {name: 'BMW', parts: {d: 10, e: 20}};
-b = {...a};
+const a = { name: 'BMW', parts: { d: 10, e: 20 } };
+b = { ...a };
 b.name = 'Jaguar';
 b.parts.d = 20;
-console.log(b.parts.d); 
+console.log(b.parts.d);
 console.log(a.parts.d);
 
-const a = {name: 'BMW', parts: {d: 10, e: 20}};
-b = {...a};
+const a = { name: 'BMW', parts: { d: 10, e: 20 } };
+b = { ...a };
 b.name = 'Jaguar';
 
-let b = {parts: {...a.parts}}
-let b = {...a, parts: {...a.parts}}
+let b = { parts: { ...a.parts } }
+let b = { ...a, parts: { ...a.parts } }
 b.parts.d = 20;
-console.log(b.parts.d); 
+console.log(b.parts.d);
 console.log(a.parts.d);
 console.log(a);
 console.log(b);
@@ -23,14 +23,14 @@ console.log(b);
 // refrence assignment
 // deep copy
 
-const array = [1,5,6];
+const array = [1, 5, 6];
 const sortedArray = array.sort();
-for (var i = 0; i <sortedArray ;i++ )
-console.log(sortedArray)
+for (var i = 0; i < sortedArray; i++)
+  console.log(sortedArray)
 
 
-const person = {name :"shazia",age:"23"};
-const copiedPerson = {...person};
+const person = { name: "shazia", age: "23" };
+const copiedPerson = { ...person };
 console.log(person);
 console.log(copiedPerson);
 // copiedPerson = person;
@@ -41,9 +41,9 @@ console.log(person);
 console.log(copiedPerson);
 
 // shallow copy and deep copy with spread operators with arrays and objects
-const person = {name :"shazia",age:"23"};
+const person = { name: "shazia", age: "23" };
 // Deep copy
-const copiedPerson = {...person}; //will not effect copied Array
+const copiedPerson = { ...person }; //will not effect copied Array
 person.name = "najar"
 console.log(person);
 console.log(copiedPerson);
@@ -53,9 +53,9 @@ person.name = "ali"
 console.log(person);
 console.log(copiedPerson);
 
-const person = {name :"shazia",age:"23",hobbies:{one :"playing",two:"reading"}};
+const person = { name: "shazia", age: "23", hobbies: { one: "playing", two: "reading" } };
 // deep copy for name property and shallow copy for hobbies
-const copiedPerson = {...person};
+const copiedPerson = { ...person };
 console.log(person);
 console.log(copiedPerson);
 // again deep copy for name and shallow copy for hobbies
@@ -65,17 +65,29 @@ console.log(person);
 console.log(copiedPerson);
 
 // creating Deep copy for hobbies also
-const copiedPerson = {...person,hobbies:{...person.hobbies}};
+const copiedPerson = { ...person, hobbies: { ...person.hobbies } };
 person.hobbies.one = "surfing";
 copiedPerson.hobbies.one = "playing";
 console.log(person);
 console.log(copiedPerson);
 
 // deep copy using parsing and stringify
-const dataStore ={name : "urmella",dairy:{write:"secrets",schedule:"timeTable"}};
+const dataStore = { name: "urmella", dairy: { write: "secrets", schedule: "timeTable" } };
 const copiedDataStore = JSON.parse(JSON.stringify(dataStore));
 console.log(dataStore);
 console.log(copiedDataStore);
 dataStore.dairy.write = "code";
 console.log(dataStore);
 console.log(copiedDataStore);
+
+// concept of var inside the function
+var a = 10;
+
+function func1() {
+  console.log(a); // undefined because hoisting concept;
+  var a = "hello world";
+  //  console.log(a);
+}
+
+func1();
+console.log(a); // 10
